60d6e2c74d007ef5c78a6ccd4cd9cfc86f6be243
unknown <abiramisaravanansrva@gmail.com>
2023-03-28 16:07:27 +0530

    First Commit

---
+++ src/main/java/com/example/application_of_hospital/ApplicationOfHospitalApplication.java
@@ -0,0 +1,14 @@
+package com.example.application_of_hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationOfHospitalApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ApplicationOfHospitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/application_of_hospital/ApplicationOfHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.application_of_hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationOfHospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
